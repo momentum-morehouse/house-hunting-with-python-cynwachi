a305e94813a3f913751872681daf2384e68e9fa6
cynwachi <christinenwachi@gmail.com>
2020-09-29 20:16:59 -0500

    small changes

--- house_hunting.py
+++ house_hunting.py
@@ -10,9 +10,18 @@ portion_saved =float(input("how much do you want to save each month"))
 
 def how_long_money_needed(downpayment, total_cost, salary, po):
     current_savings= 0 
+
+    i = 0 
     # each month takes current savings and add earned interest and salary saved
     while current_savings < downpayment:
-        current_savings = current_savings + salary * portion_saved / 12 + current_savings* 0.04 / 12
+        current_savings += 
+        monthly_salary_saved
+        investent_return = current_savins * (.04/12)
+        current_saings += investment_return
+        i + 1
+        
+        print("Number of months:", i)
+        current_savings + salary * portion_saved / 12 + current_savings* 0.04 / 12
     #stop when the amont of savings is equal to the downpayment 
     # count the number of months that pass 
     return months
