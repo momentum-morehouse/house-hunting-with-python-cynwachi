dbb96c04130cc74d95a2d3137b04c4e6fed16ae3
cynwachi <christinenwachi@gmail.com>
2020-10-15 01:06:06 -0500

    commented out in least steps

--- house_hunting.py
+++ house_hunting.py
@@ -1,28 +1,35 @@
-annual_salary = float("What is your annual Salary ")
+#Write a python script that will calculate how many months it will take to save enough money for a down payment
+#Ask for input = Variables annual_salary, portion_saved, total_cost,
+#Print output = annual_salary, portion_saved (.15), total_cost
 
-total_cost = float(input("what is the cost of the dream home "))
-
-print(total_cost)
-
-downpayment = 0.25* total_cost 
-portion_saved =float(input("how much do you want to save each month"))
-
-
-def how_long_money_needed(downpayment, total_cost, salary, po):
-    current_savings= 0 
-
-    i = 0 
-    # each month takes current savings and add earned interest and salary saved
-    while current_savings < downpayment:
-        current_savings += 
-        monthly_salary_saved
-        investent_return = current_savins * (.04/12)
-        current_saings += investment_return
-        i + 1
-        
-        print("Number of months:", i)
-        current_savings + salary * portion_saved / 12 + current_savings* 0.04 / 12
-    #stop when the amont of savings is equal to the downpayment 
-    # count the number of months that pass 
-    return months
+"""
+- Retrieve user input. Look at `input()` if you need help with getting user input. For this problem set, you can assume that users will enter valid input (e.g. they won’t enter a string when you expect a number.)
+- Initialize some state variables. You should decide what information you need. Be careful about values that represent annual amounts and those that represent monthly amounts.
+- Try different inputs and see how long it takes to save for a down payment.
     
+"""
+
+#=> total_cost = Cost of Home
+#=> portion_down_payment = Portion of cost needed for downpayment (=0.25/25%)
+#=> current_savings = Amount saved so far 
+# Current savings starts with = 0
+
+# Annual 'r' "Annual Rate" = 0.04/4% 
+# At the end of each month I receive an addtional => current_savings*r/12 see last line for formula 
+
+
+
+
+#=> annual_salary = annual salary
+
+#portion_saved in decimal form  
+
+
+
+
+#=> At the end of each month my savings will increase by the return on my investment ((current_savings*0.04)/12)
+
+
+
+
+#=> W
